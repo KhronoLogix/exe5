398c5eb593e1d97842782a3d167cc68e2b72be79
Sprite <139699268+KhronoLogix@users.noreply.github.com>
2025-02-11 14:15:45 -0500

    Update AddressBook.java

--- exe5/src/AddressBook.java
+++ exe5/src/AddressBook.java
@@ -7,18 +7,20 @@ public class AddressBook {
     public static void main(String[] args) {
         int exit = 1;
         while (exit != 0) {
-            System.out.println("Name:"); String name = input.nextLine();
-            System.out.println("Email;"); String email = input.nextLine();
+            System.out.println("Name:");
+            String name = input.nextLine();
+            System.out.println("Email;");
+            String email = input.nextLine();
             blackBook.add(new Person(name, email));
-            System.out.println("do you wanna exit? Y/N: "); String userIn = input.nextLine();
-            if(userIn == "Y"){
-                exit = 0;
-            } else if (userIn == "y") {
+            System.out.println("do you wanna exit? Y/N: ");
+            String userIn = input.nextLine();
+            if(userIn.equalsIgnoreCase("Y")){
                 exit = 0;
             }else{
                 System.out.println("add a new person!");
             }
         }
+        System.out.println(blackBook);
         System.out.println("bye bye!!!");
         input.nextLine();
     }//ADD ROBLOXXXXXXX
