f3c42ccb3e271223f69df208c62f8326cc0f8871
Sprite <139699268+KhronoLogix@users.noreply.github.com>
2025-02-11 14:01:11 -0500

    Add files via upload

---
+++ exe5/src/AddressBook.java
@@ -0,0 +1,25 @@
+//Dominic Cruz / 11/2/25 / Exercise 5
+//ADD ROBLOXXXXXXX
+import java.util.*;//ADD ROBLOXXXXXXX
+public class AddressBook {
+    static Scanner input = new Scanner(System.in);
+    static ArrayList<Person> blackBook = new ArrayList<>();
+    public static void main(String[] args) {
+        int exit = 1;
+        while (exit != 0) {
+            System.out.println("Name:"); String name = input.nextLine();
+            System.out.println("Email;"); String email = input.nextLine();
+            blackBook.add(new Person(name, email));
+            System.out.println("do you wanna exit? Y/N: "); String userIn = input.nextLine();
+            if(userIn == "Y"){
+                exit = 0;
+            } else if (userIn == "y") {
+                exit = 0;
+            }else{
+                System.out.println("add a new person!");
+            }
+        }
+        System.out.println("bye bye!!!");
+        input.nextLine();
+    }//ADD ROBLOXXXXXXX
+}//ADD ROBLOXXXXXXX
